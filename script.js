6c321ee4d96a9912d7ccd458537f9ec7ff00331a
Sergei-rgb@ <sergei25102007@bk.ru>
2024-10-05 15:41:12 +0400

    main

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('loading').style.display = 'none';
+    }, 2000);
+};
+document.getElementById('Foto').addEventListener('click', function() {
+    showLoading();
+    setTimeout(function() {
+        hideLoading();
+        alert('Загружаем фото танцев!');
+    }, 3000);
+});
+
+document.getElementById('Video').addEventListener('click', function() {
+    showLoading();
+    setTimeout(function() {
+        hideLoading();
+        alert('Загружаем видео танцев!');
+    }, 3000);
+});
+
+function showLoading() {
+    document.getElementById('loading').classList.remove('hidden');
+};
+
+function hideLoading() {
+    document.getElementById('loading').classList.remove('hidden');
+};
