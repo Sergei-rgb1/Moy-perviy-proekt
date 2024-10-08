ad52f8cceb2049bddf3485f7d6bca4c2b5657a49
Sergei-rgb@ <sergei25102007@bk.ru>
2024-10-08 18:39:43 +0400

    main

--- script.js
+++ script.js
@@ -27,28 +27,28 @@ function hideLoading() {
     document.getElementById('loading').classList.remove('hidden');
 };  
 
-document.getElementById('Foto').addEventListener('click', function() {
-    document.getElementById('Tan').style.display = 'none';
-    document.querySelector('div').style.display = 'none';
+// document.getElementById('Foto').addEventListener('click', function() {
+//     document.getElementById('Tan').style.display = 'none';
+//     document.querySelector('div').style.display = 'none';
 
-    document.getElementById('newContent').style.display = 'block';
-});
+//     document.getElementById('newContent').style.display = 'block';
+// });
 
-document.getElementById('Video').addEventListener('click', function() {
-    document.getElementById('Tan').style.display = 'none';
-    document.querySelector('div').style.display = 'none';
+// document.getElementById('Video').addEventListener('click', function() {
+//     document.getElementById('Tan').style.display = 'none';
+//     document.querySelector('div').style.display = 'none';
 
-    document.getElementById('newContent').style.display = 'block';
-});
+//     // document.getElementById('newContent').style.display = 'block';
+// });
 
-window.addEventListener('popstate', function(event) {
-    if (event.state && event.state.page === 'new') {
-    document.getElementById('Tan').style.display = 'none';
-    document.getElementById('mainContent').style.display = 'none';
-    document.getElementById('newContent').style.display = 'block';
-    } else {
-     document.getElementById('Tan').style.display = 'none';
-     document.getElementById('mainContent').style.display = 'none';
-     document.getElementById('newContent').style.display = 'block';
-    }
-});
+// // window.addEventListener('popstate', function(event) {
+// //     if (event.state && event.state.page === 'new') {
+// //     document.getElementById('Tan').style.display = 'none';
+// //     document.getElementById('mainContent').style.display = 'none';
+// //     document.getElementById('newContent').style.display = 'block';
+// //     } else {
+// //      document.getElementById('Tan').style.display = 'none';
+// //      document.getElementById('mainContent').style.display = 'none';
+// //      document.getElementById('newContent').style.display = 'block';
+// //     }
+// // });
