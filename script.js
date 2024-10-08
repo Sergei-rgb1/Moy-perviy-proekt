3c2a26d9716dc29dfdd6511f36240fbec89b5e4c
Sergei-rgb@ <sergei25102007@bk.ru>
2024-10-08 15:20:38 +0400

    main

--- script.js
+++ script.js
@@ -26,3 +26,29 @@ function showLoading() {
 function hideLoading() {
     document.getElementById('loading').classList.remove('hidden');
 };  
+
+document.getElementById('Foto').addEventListener('click', function() {
+    document.getElementById('Tan').style.display = 'none';
+    document.querySelector('div').style.display = 'none';
+
+    document.getElementById('newContent').style.display = 'block';
+});
+
+document.getElementById('Video').addEventListener('click', function() {
+    document.getElementById('Tan').style.display = 'none';
+    document.querySelector('div').style.display = 'none';
+
+    document.getElementById('newContent').style.display = 'block';
+});
+
+window.addEventListener('popstate', function(event) {
+    if (event.state && event.state.page === 'new') {
+    document.getElementById('Tan').style.display = 'none';
+    document.getElementById('mainContent').style.display = 'none';
+    document.getElementById('newContent').style.display = 'block';
+    } else {
+     document.getElementById('Tan').style.display = 'none';
+     document.getElementById('mainContent').style.display = 'none';
+     document.getElementById('newContent').style.display = 'block';
+    }
+});
