ef93aa353c45e638d9d5c2d985b807c73c1d1ba0
Sergei-rgb@ <sergei25102007@bk.ru>
2024-10-14 20:35:14 +0400

    main

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById('loading').style.display = 'none';
-    }, 2000);
-};
-document.getElementById('Foto').addEventListener('click', function() {
-    showLoading();
-    setTimeout(function() {
-        hideLoading();
-        alert('Загружаем фото танцев!');
-    }, 3000);
-});
+// window.onload = function() {
+//     setTimeout(function() {
+//         document.getElementById('loading').style.display = 'none';
+//     }, 2000);
+// };
 
-document.getElementById('Video').addEventListener('click', function() {
-    showLoading();
-    setTimeout(function() {
-        hideLoading();
-        alert('Загружаем видео танцев!');
-    }, 3000);
-});
+
+
 
 function showLoading() {
     document.getElementById('loading').classList.remove('hidden');
@@ -27,28 +15,3 @@ function hideLoading() {
     document.getElementById('loading').classList.remove('hidden');
 };  
 
-// document.getElementById('Foto').addEventListener('click', function() {
-//     document.getElementById('Tan').style.display = 'none';
-//     document.querySelector('div').style.display = 'none';
-
-//     document.getElementById('newContent').style.display = 'block';
-// });
-
-// document.getElementById('Video').addEventListener('click', function() {
-//     document.getElementById('Tan').style.display = 'none';
-//     document.querySelector('div').style.display = 'none';
-
-//     // document.getElementById('newContent').style.display = 'block';
-// });
-
-// // window.addEventListener('popstate', function(event) {
-// //     if (event.state && event.state.page === 'new') {
-// //     document.getElementById('Tan').style.display = 'none';
-// //     document.getElementById('mainContent').style.display = 'none';
-// //     document.getElementById('newContent').style.display = 'block';
-// //     } else {
-// //      document.getElementById('Tan').style.display = 'none';
-// //      document.getElementById('mainContent').style.display = 'none';
-// //      document.getElementById('newContent').style.display = 'block';
-// //     }
-// // });
